3b330a2dfdd1c0baeb38790145dace15de626682
Ainur <nur.php@gmail.com>
2015-12-10 16:21:09 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+open graph cache flusher
+======
+
+
+# Installation
+`npm install -g ogflush`
+
+# Usage
+```bash
+ogflush -u "http://some.url/"
+ogflush -f file.txt
+```
+
+# Supported networks
+`facebook` (facebook.com)
+`vk` (vk.com)
---
+++ ogflush.js
@@ -0,0 +1,39 @@
+#! /usr/bin/env node
+var request = require('request');
+var fs = require('fs');
+
+var argv = require('minimist')(process.argv.slice(2));
+
+var urls = [];
+if(argv.u){
+  urls.push(argv.u);
+}
+if(argv.f) {
+  var _file = fs.readFileSync(argv.f, {encoding: 'utf8'});
+  urls = urls.concat(_file.split("\n"));
+}
+urls = urls.filter(function(u){return !!u});
+
+var _q = function(url, params, log_name){
+  request({url: url, qs: params}, function(err, response, body) {
+    if(err) { console.log(err); return; }
+  });
+}
+var flushFb = function (url) {
+  _q('https://graph.facebook.com/', {
+    id: url,
+    scrape: true
+  }, url);
+};
+
+var flushVK = function (url) {
+  _q('https://api.vk.com/method/pages.clearCache', {
+    url: url
+  }, url);
+};
+
+urls.forEach(function(url){
+  flushFb(url);
+  flushVK(url);
+  console.log('ok', url);
+});
